7a6af1dfcd12ba08b5ce1f499757ea45e79fecf8
kesarwanishubhi <shubhikesarwani1234@gmail.com>
2024-08-31 18:19:24 +0530

    new changes

--- 1st/1.c
+++ 1st/1.c
@@ -10,47 +10,46 @@ c. FIFO (mkfifo Library Function or mknod system call)
 
 ======================================================================================================================================================================
 Sample Output:
+ln -s s.txt soft
+ln s.txt hard
+mkfifo fifo
+
 
 ======================================================================================================================================================================
 */
 
-#include<fcntl.h>
-#include<unistd.h>
-#include<stdio.h>
-#include<sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]){
-    if(argc < 3){
-        perror("Please enter the source file and destination file names");
+int main(int argc, char* argv[]) {
+    if (argc < 5) {
+        fprintf(stderr, "Usage: %s <source_file> <hardlink_file> <symlink_file> <fifo_file>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-    else{
-        //Creating a hard link file
-        if(link(argv[1], argv[2]) == -1){
-            perror("link");
-        }
-        else{
-            printf("Hard link file created successfully\n");
-        }
 
-        //Creating a softlink file
-        if(symlink(argv[1], argv[3]) == -1){
-            perror("symlink");
-        }
-        else{
-            printf("Soft link file created successfully\n");
-        }
-
-        //Creating a FIFO file
-        //if(mkfifo("myfifofile", S_IFIFO|S_IRWXU) == -1){
-        //  perror("mkfifo");
-        //}
-
-        if(mknod(argv[4], S_IFIFO, 0) == -1){
-            perror("Failed to create named pipe");
-        }
-        else {
-            printf("FIFO file created successfully\n");
-        }
+    // Creating a hard link file
+    if (link(argv[1], argv[2]) == -1) {
+        perror("Error creating hard link");
+    } else {
+        printf("Hard link file '%s' created successfully\n", argv[2]);
     }
+
+    // Creating a symbolic link file
+    if (symlink(argv[1], argv[3]) == -1) {
+        perror("Error creating symbolic link");
+    } else {
+        printf("Symbolic link file '%s' created successfully\n", argv[3]);
+    }
+
+    // Creating a FIFO file
+    if (mkfifo(argv[4], S_IFIFO | S_IRWXU) == -1) {
+        perror("Error creating FIFO");
+    } else {
+        printf("FIFO file '%s' created successfully\n", argv[4]);
+    }
+
     return 0;
 }
